b057cd6ed5b97b5dd7d8b0f6adc8614e92d2a43d
bavdu <bavduer@163.com>
2019-05-28 09:54:05 +0800

    functions

---
+++ functions01.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# author: liuchao
+# date: 2019/05/28
+
+Println() {
+  echo "hello world"
+}
+
+function Echoln() {
+  echo "hello kitty"
+}
+
+Echoln
+Println
---
+++ functions02.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+#
+# author: liuchao
+# date: 2019/05/28
+
+Install_Nginx() {
+cat <<-EOF >/etc/yum.repos.d/nginx.repo 
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+EOF
+
+if [ -f /etc/yum.repos.d/nginx.repo ];then
+  yum -y install nginx
+  if [ $? -eq 0 ];then
+    systemctl start nginx
+    systemctl enable nginx
+  else
+    echo "nginx service is failed install."
+  fi
+else
+  echo "nginx's yumrepo is not exists."
+fi
+}
+
+Php_install() {
+  rpm -qa | grep php-fpm
+  if [ $? -eq 0 ];then
+    echo "php-fpm installed."
+  else
+    yum -y install php php-fpm php-mysql php-gd php-mbstring php-mcrypt php-devel php-curl
+  fi
+}
+
+while :
+do
+cat <<-EOF
+	+===================+
+	|   Install tools   |
+        +===================+
+	|  1.install nginx  |
+ 	|  2.install php    |
+	|  3.Quit	    |
+	+===================+
+EOF
+
+read -p "please Input your choose: " choose
+case $choose in
+  1)
+    Install_Nginx 
+    ;;
+  2)
+    Php_install
+    ;;
+  3)
+    exit
+    ;;
+  *)
+    echo "Please choose (1, 2, 3) or getout!~"
+esac
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
